d3af09390a1fb6910b64e328a6bf9bdeb96fd1ed
rqlynx <47145613+rqlynx@users.noreply.github.com>
2019-05-31 10:50:00 +0900

    list3.15

--- MyBootApp/src/main/java/jp/abc/HeloController.java
+++ MyBootApp/src/main/java/jp/abc/HeloController.java
@@ -1,54 +1,20 @@
 package jp.abc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HeloController {
-	String[] names = { "tuyano", "hanako", "taro", "sachiko", "ichiro" };
-	String[] mails = { "syoda@tuuyano.com", "hanako@flower", "taro@yamda", "sachiko@happy", "ichiro@baseball" };
 
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {
-
-		return new DataObject(id,names[id],mails[id]);
-	}
-}
-
-class DataObject {
-	private int id;
-	private String name;
-	private String value;
-
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		int res = 0;
+		for (int i = 1; i <= num; i++) {
+			res += i;
+		}
+		model.addAttribute("msg", "total: " + res);
+		return "index";
 	}
 }
